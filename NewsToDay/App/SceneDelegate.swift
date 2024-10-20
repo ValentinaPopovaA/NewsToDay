0759c6779afc41a20525eaa1baab07e7658b5b84
Valentina Popova <info@valentina-popova.ru>
2024-10-20 21:59:17 +0300

    Start project

---
+++ NewsToDay/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  NewsToDay
+//
+//  Created by Валентина Попова on 20.10.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        window.rootViewController = rootViewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
