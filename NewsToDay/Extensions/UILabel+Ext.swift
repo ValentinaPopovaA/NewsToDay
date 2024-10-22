4a5a49e7e900c75653cfdefdffd1c180614b9aeb
Valentina Popova <info@valentina-popova.ru>
2024-10-22 16:29:43 +0300

    Fix UILabel+Ext

--- NewsToDay/Extensions/UILabel+Ext.swift
+++ NewsToDay/Extensions/UILabel+Ext.swift
@@ -10,12 +10,14 @@ import UIKit
 extension UILabel {
     static func createLabel(text: String,
                             fontSize: CGFloat,
+                            textColor: UIColor,
                             isBold: Bool = false,
                             textAlignment: NSTextAlignment = .center,
                             numberOfLines: Int = 0) -> UILabel {
         let label = UILabel()
         label.text = text
         label.font = isBold ? UIFont.interBold : UIFont.interRegular
+        label.textColor = textColor
         label.textAlignment = textAlignment
         label.numberOfLines = numberOfLines
         label.translatesAutoresizingMaskIntoConstraints = false
