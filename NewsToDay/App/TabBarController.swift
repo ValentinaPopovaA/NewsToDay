24ea9202db2f14a1bae7d3a4f2f4901b3cf37b15
Alexander Svyatov <whiterabbit1232@gmail.com>
2024-10-23 17:46:49 +0300

    fix tabBar icons and border

--- NewsToDay/App/TabBarController.swift
+++ NewsToDay/App/TabBarController.swift
@@ -9,6 +9,16 @@ enum Tabs: Int {
 
 final class TabBarController: UITabBarController {
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let newHeight: CGFloat = 100
+        var tabBarFrame = tabBar.frame
+        tabBarFrame.size.height = newHeight
+        tabBarFrame.origin.y = view.frame.height - newHeight
+        tabBar.frame = tabBarFrame
+    }
+    
     override func viewDidLoad() {
         configure()
     }
@@ -18,26 +28,30 @@ final class TabBarController: UITabBarController {
         tabBar.tintColor = .purplePrimary
         tabBar.barTintColor = .grayLight
         tabBar.backgroundColor = .white
+        tabBar.layer.borderColor = UIColor.grayLight?.cgColor ?? UIColor.gray.cgColor
+        tabBar.layer.borderWidth = 1
         tabBar.layer.cornerRadius = 12
         tabBar.layer.masksToBounds = true
         
         let browseViewController = UIViewController()
+        browseViewController.view.backgroundColor = .cyan
         let categoriesViewController = UIViewController()
         let bookmarksViewController = UIViewController()
         let profileViewController = UIViewController()
         
         browseViewController.tabBarItem = UITabBarItem(title: nil,
-                                                       image: UIImage(systemName: "house"),
+                                                       image: UIImage(named: "browse"),
                                                        tag: Tabs.browse.rawValue)
         categoriesViewController.tabBarItem = UITabBarItem(title: nil,
-                                                           image: UIImage(systemName: "square.grid.2x2"),
+                                                           image: UIImage(named: "categories"),
                                                            tag: Tabs.categories.rawValue)
         bookmarksViewController.tabBarItem = UITabBarItem(title: nil,
-                                                          image: UIImage(systemName: "bookmark"),
+                                                          image: UIImage(named: "bookmarks"),
                                                           tag: Tabs.bookmarks.rawValue)
         profileViewController.tabBarItem = UITabBarItem(title: nil,
-                                                        image: UIImage(systemName: "person"),
+                                                        image: UIImage(named: "profile"),
                                                         tag: Tabs.profile.rawValue)
+        
         
         setViewControllers([
             browseViewController,
