7cfdfdc12b53fbc4ddb79f393a7c0a0d1503c9aa
Bakgeldi Alkhabay <74294553+bakgeldia@users.noreply.github.com>
2024-10-29 16:32:14 +0500

    Changed root controller

--- NewsToDay/App/SceneDelegate.swift
+++ NewsToDay/App/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let rootViewController = ProfileViewController()
+        let rootViewController = TabBarController()
         
         window.rootViewController = rootViewController
         self.window = window
